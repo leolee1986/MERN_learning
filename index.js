78e738f604d1de53d5b0b5a00c8639af6cec73cf
Li <leolee1986@gmail.com>
2017-12-22 13:44:05 -0700

    install express, add google oauth strategy

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require('./config/keys.js');
 
 const app = express();
 
-//route hander
-// index route
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+// setup google oauth
+passport.use(new GoogleStrategy({
+  clientID:keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL:'/auth/google/callback'
+  }, (accessToken) => {
+      console.log(accessToken);
+  })
+);
+
+
+
+
 
 //listen to PORT, for Heroku will be process.env.PORT, for local env, 5000
 const PORT = process.env.PORT || 5000;
