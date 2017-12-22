6ee28e79e666c37e582d08da6483e1d6091c647b
Li <leolee1986@gmail.com>
2017-12-22 14:48:38 -0700

    add route handler to handle google oauth, add dev env, install nodemon

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require('./config/keys.js');
+const keys = require('./config/keys');
 
 const app = express();
 
 // setup google oauth
 passport.use(new GoogleStrategy({
-  clientID:keys.googleClientID,
+  clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
-  callbackURL:'/auth/google/callback'
-  }, (accessToken) => {
-      console.log(accessToken);
+  callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  console.log('accessToken :', accessToken);
+  console.log('refreshToken :', refreshToken);
+  console.log('profile :', profile);
+}));
+
+// route to handle authentication
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
   })
 );
 
-
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 
