7a7b85d806d0eade75d421ab70bfa2e25f31703a
Li <leolee1986@gmail.com>
2017-12-29 13:08:07 -0700

    create dev.js and prod.js for different env, modify keys.js to commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// keys.js
+if (process.env.NODE_ENV === 'production') {
+    // production - return prod keys
+    module.exports = require('./prod');
+} else {
+    // dev - return dev keys
+    module.exports = require('./dev');
+}
