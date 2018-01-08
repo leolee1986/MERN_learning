cbc13a9aa837da336d267503eb9c8855f009eef7
Li <leolee1986@gmail.com>
2018-01-08 15:16:24 -0700

    add new proxy rule

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,14 +3,16 @@ import React , { Component } from 'react';
 class Header extends Component {
     render () {
         return(
-            <nav>
+            <div className="container">
+                <nav>
                 <div className="nav-wrapper">
                 <a href="#" className="left brand-logo">Emaily</a>
                 <ul className="right">
                     <li><a href="sass.html">Login with Google</a></li>
                 </ul>
                 </div>
-            </nav>
+                </nav>
+            </div>
         );
     }
 }
